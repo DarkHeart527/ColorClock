38f100edf475a985bee6a27239daf5adcf1a629c
DarkHeart527 <darkheart527s@gmail.com>
2016-09-29 14:40:01 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 function submit() {
-  document.getElementById('colorAllowed').style = document.getElementById('textColor').value;
-  document.getElementById('colorAllowed').style = document.getElementById('backgroundColor').value;
+  document.getElementById('colorAllowed').style = "color: " + document.getElementById('textColor').value;
+  document.getElementById('colorAllowed').style = "background-color: " + document.getElementById('backgroundColor').value;
   alert('Styling Has Been Changed To: | Text Color - ' + document.getElementById('textColor').value + ' | Background Color - ' + document.getElementById('backgroundColor').value + ' |');
 }
 

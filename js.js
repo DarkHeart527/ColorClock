48687912a7329cc36c7dc626dcdcc4d73226683d
DarkHeart527 <darkheart527s@gmail.com>
2016-09-29 14:27:19 -0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,9 @@
+function submit() {
+  document.getElementById('colorAllow').style = document.getElementById('textColor').value;
+  document.getElementById('colorAllowed').style = document.getElementById('backgroundColor').value;
+  alert('Styling Has Been Changed To: | Text Color - ' + document.getElementById('textColor').value; + ' | Background Color - ' + document.getElementById('backgroundColor').value + ' |';
+}
+
+function clock() {
+  
+}

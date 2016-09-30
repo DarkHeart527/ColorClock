ce823ffa8934b04c989b0de03fc7ddc091ae8501
DarkHeart527 <darkheart527s@gmail.com>
2016-09-30 13:59:31 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,14 @@
 function submit() {
-  document.getElementById('colorAllowed').style = "background-color: " + document.getElementById('backgroundColor').value + ";" + "color: " + document.getElementById('textColor').value;
-  alert('Styling Has Been Changed To: | Text Color - ' + document.getElementById('textColor').value + ' | Background Color - ' + document.getElementById('backgroundColor').value + ' |');
+  document.getElementById('colorAllowed').style = "color: " + document.getElementById('textColor').value;
+  document.getElementById('backGround').style = "background-color: " + document.getElementById('backgroundColor').value;
+  setTimeout( function() { alert('Styling Has Been Changed To: | Text Color - ' + document.getElementById('textColor').value + ' | Background Color - ' + document.getElementById('backgroundColor').value + ' |'); }, 500);
 }
 
 function clock() {
-  
+    var d = new Date();
+    var mi = d.getMilliseconds();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours() - 12;
+    document.getElementById('colorAllowed').innerHTML = h + ":" + m + ":" + s + "." + mi;
 }
